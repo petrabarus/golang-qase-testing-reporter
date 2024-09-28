f80e428f6ffd50025a1ce19615af2bc80d5a7df2
Petra Barus <petra.barus@gmail.com>
2024-09-28 11:15:12 +0700

    add version

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+// go-qase-testing-reporter is a tool to report test results to Qase.
+// Since Go testing does not have a built-in testing event listener,
+// we need to parse the test output and report the results to Qase.
+// To run this file locally, execute:
+// go run . --api-token <qase-api-token> --project <qase-project-name> --run-title <qase-run-title> <path/to/test-results.jsonl>
 package main
 
 import (
@@ -10,12 +15,19 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"runtime/debug"
 	"strconv"
 	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	qase "go.qase.io/client"
+)
+
+var (
+	Version = "dev"
+	Commit  = "none"
+	Date    = "unknown"
 )
 
 type Config struct {
@@ -42,6 +54,21 @@ type ReportResult struct {
 	TimeMs     int64
 }
 
+type ReportResultOutput struct {
+	TestCaseId int64
+	Status     string
+}
+
+type ReportOutput struct {
+	RunId    int32                 `json:"run_id"`
+	TestRuns []ReportOutputTestRun `json:"test_runs"`
+}
+
+type ReportOutputTestRun struct {
+	TestCaseId int64  `json:"test_case_id"`
+	Status     string `json:"status"`
+}
+
 var (
 	ctx context.Context
 
@@ -54,7 +81,7 @@ var (
 Since Go testing does not have a built-in testing event listener, 
 we need to parse the test output and report the results to Qase.
 `,
-		Args:             cobra.ExactArgs(1),
+		Args:             cobra.MaximumNArgs(1),
 		ArgAliases:       []string{"filename"},
 		PersistentPreRun: preRun,
 		Run:              RunCommand,
@@ -74,6 +101,9 @@ func init() {
 	cmd.Flags().StringP("project", "p", "", "Qase project name")
 	cmd.Flags().StringP("api-token", "t", "", "Qase API token")
 	cmd.Flags().StringP("run-title", "r", "", "Qase run title")
+
+	// add --version flag
+	cmd.Flags().BoolP("version", "v", false, "Print version")
 
 	viper.BindPFlag("project", cmd.Flags().Lookup("project"))
 	viper.BindPFlag("api_token", cmd.Flags().Lookup("api-token"))
@@ -99,9 +129,11 @@ func preRun(cmd *cobra.Command, args []string) {
 	if err != nil {
 		log.Fatalf("Unable to read Viper options into configuration: %v", err)
 	}
-	config.Filename = args[0]
+	if len(args) > 0 {
+		config.Filename = args[0]
+	}
 
-	log.Printf("Config: %+v", config)
+	//log.Printf("Config: %+v", config)
 	ctx = context.Background()
 
 	initQaseClient()
@@ -114,8 +146,20 @@ func initQaseClient() {
 }
 
 func RunCommand(cmd *cobra.Command, args []string) {
+	if printVersion(cmd) {
+		return
+	}
+
+	if config.Filename == "" {
+		fmt.Fprintln(os.Stderr, "Error: filename is required")
+		// print usage
+		cmd.Usage()
+		return
+	}
+
 	var err error
-	fmt.Println("Running go-qase-testing-reporter")
+	var output ReportOutput
+	//fmt.Println("Running go-qase-testing-reporter")
 	results, err := processFile(config.Filename)
 	if err != nil {
 		log.Fatalf("Failed to process file: %v", err)
@@ -127,7 +171,7 @@ func RunCommand(cmd *cobra.Command, args []string) {
 		log.Fatalf("Failed to create test run: %v", err)
 	}
 
-	err = createTestRunResults(id, results)
+	testRunResultOutputs, err := createTestRunResults(id, results)
 	if err != nil {
 		log.Fatalf("Failed to create test run result: %v", err)
 	}
@@ -136,6 +180,51 @@ func RunCommand(cmd *cobra.Command, args []string) {
 	if err != nil {
 		log.Fatalf("Failed to complete test run: %v", err)
 	}
+
+	output = createOutput(id, testRunResultOutputs)
+	printOutput(output)
+}
+
+func printVersion(cmd *cobra.Command) (shouldExit bool) {
+	shouldPrintVersion, _ := cmd.Flags().GetBool("version")
+	if !shouldPrintVersion {
+		return false
+	}
+	version, ok := getVersionFromBuildInfo()
+	if ok {
+		version = fmt.Sprintf("%s-%s-%s", Version, Commit, Date)
+	}
+	fmt.Printf("go-qase-testing-reporter %s\n", version)
+	return true
+}
+
+// Enable to generate version if being installed using `go install`
+func getVersionFromBuildInfo() (version string, ok bool) {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	version = buildInfo.Main.Version
+	// find `vcs.time` and `vcs.revision`
+	for _, setting := range buildInfo.Settings {
+		if setting.Key == "vcs.time" {
+			// truncate to 10 chars if possible
+			date := setting.Value
+			if len(date) > 10 {
+				date = date[:10]
+			}
+			version = fmt.Sprintf("%s-%s", version, date)
+		}
+		if setting.Key == "vcs.revision" {
+			// truncate to 8 chars if possible
+			commit := setting.Value
+			if len(commit) > 8 {
+				commit = commit[:8]
+			}
+			version = fmt.Sprintf("%s-%s", version, commit)
+		}
+	}
+	return
 }
 
 func createNewRun(results []ReportResult) (runId int32, err error) {
@@ -163,7 +252,8 @@ func createNewRun(results []ReportResult) (runId int32, err error) {
 	return
 }
 
-func createTestRunResults(runId int32, results []ReportResult) (err error) {
+func createTestRunResults(runId int32, results []ReportResult) (testRunResultOutputs []ReportResultOutput, err error) {
+	testRunResultOutputs = make([]ReportResultOutput, 0)
 	qaseResults := make([]qase.ResultCreate, 0)
 	for _, result := range results {
 		qaseResult := qase.ResultCreate{
@@ -177,6 +267,10 @@ func createTestRunResults(runId int32, results []ReportResult) (err error) {
 			qaseResult.Comment = fmt.Sprintf("Package: %v", result.Package)
 		}
 		qaseResults = append(qaseResults, qaseResult)
+		testRunResultOutputs = append(testRunResultOutputs, ReportResultOutput{
+			TestCaseId: int64(result.TestCaseId),
+			Status:     result.Status,
+		})
 	}
 
 	qaseResp, httpResp, err := qaseClient.ResultsApi.CreateResultBulk(ctx, qase.ResultCreateBulk{
@@ -334,4 +428,29 @@ func ParseQaseId(test string) (int, error) {
 		return 0, errors.New("failed to parse Qase ID")
 	}
 	return qaseId, nil
+}
+
+func createOutput(runId int32, testRunResultOutputs []ReportResultOutput) (output ReportOutput) {
+	output = ReportOutput{
+		RunId:    runId,
+		TestRuns: make([]ReportOutputTestRun, 0),
+	}
+	for _, testRunResultOutput := range testRunResultOutputs {
+		if testRunResultOutput.TestCaseId == 0 {
+			continue
+		}
+		output.TestRuns = append(output.TestRuns, ReportOutputTestRun{
+			TestCaseId: testRunResultOutput.TestCaseId,
+			Status:     testRunResultOutput.Status,
+		})
+	}
+	return
+}
+
+func printOutput(output ReportOutput) {
+	jsonOutput, err := json.Marshal(output)
+	if err != nil {
+		log.Fatalf("Failed to marshal output: %v", err)
+	}
+	fmt.Println(string(jsonOutput))
 }
