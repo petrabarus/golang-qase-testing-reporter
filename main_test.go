129b355fac6c6fb449f68263e709f6ceaeee7aef
Petra Barus <petra.barus@gmail.com>
2024-09-28 11:15:12 +0700

    add version

--- main_test.go
+++ main_test.go
@@ -23,27 +23,27 @@ func TestParseQaseId(t *testing.T) {
 			expected: 0,
 		},
 		{
-			name:     "QASE-3 Empty input",
+			name:     "QASE-4 Empty input",
 			input:    "",
 			expected: 0,
 		},
 		{
-			name:     "QASE-4 No Qase ID",
+			name:     "QASE-5 No Qase ID",
 			input:    "QASE-",
 			expected: 0,
 		},
 		{
-			name:     "QASE-5 Will choose the last Qase ID on multiple Qase ID - 1",
+			name:     "QASE-6 Will choose the last Qase ID on multiple Qase ID - 1",
 			input:    "QASE-123/Halohalo_QASE-456",
 			expected: 456,
 		},
 		{
-			name:     "QASE-6 Will choose the last Qase ID on multiple Qase ID - 2",
+			name:     "QASE-7 Will choose the last Qase ID on multiple Qase ID - 2",
 			input:    "QASE-123/Halohalo_QASE-456",
 			expected: 456,
 		},
 		{
-			name:     "QASE-7 Will choose the last Qase ID on multiple Qase ID - 3",
+			name:     "QASE-8 Will choose the last Qase ID on multiple Qase ID - 3",
 			input:    "QASE-123/Halohalo_QASE-789",
 			expected: 789,
 		},
